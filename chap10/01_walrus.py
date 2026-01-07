1a8b01dbaaf14dcccf8208da5bc51802cb1ad022
Atif Bashir <atifparay16@gmail.com>
2026-01-07 15:22:43 +0530

    prac

--- chap10/01_walrus.py
+++ chap10/01_walrus.py
@@ -1,7 +1,7 @@
 #using walrus operator
 if (n := len([1,2,3,4,5])) > 3:
     print(f"list is too long ({n} elements, expected <= 3)") #list is too long (5 elements, expected <= 3)
-    
+
 
     #without walrus operator
     n = len([1,2,3,4,5])
@@ -10,4 +10,5 @@ if n > 10:
 #with walrus operator
 if (n := len([1,2,3,4,5])) > 10:
     print(n)
+]
 
---
+++ chap8/function.py
@@ -0,0 +1,18 @@
+def sum(a,b):
+    print("sum", a+b)
+
+def sub(a,b):
+        print("sub", a-b)
+
+def div(a,b):
+    print("div", a/b)
+def mul(a,b):
+    print("mul", a*b)
+    
+def main():
+        sum(10,20)
+        sub(10,20)
+        div(10,20)
+        mul(10,20)
+
+main()
